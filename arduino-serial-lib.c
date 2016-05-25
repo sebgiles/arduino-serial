0c1829205632023924f17ade0e0a617a5b81d3df
sebgiles <mrsebgiles@gmail.com>
2016-05-25 13:28:50 +0200

    Added 3 functions I needed
    also some whitespace was automatically removed by Atom text editor…

--- arduino-serial-lib.c
+++ arduino-serial-lib.c
@@ -4,18 +4,26 @@
 // 2006-2013, Tod E. Kurt, http://todbot.com/blog/
 //
 
+//Modifications by Sebastian Giles (Sebgiles) marked throughout the code:
+// + added a function to peek a single char
+// + added a function to read a single char
+// + added a function to read a requested number of bytes with no timeout
+// + edited the write function to also send char* ingoring null terminators if
+//   length is given
+
+
 #include "arduino-serial-lib.h"
 
-#include <stdio.h>    // Standard input/output definitions 
-#include <unistd.h>   // UNIX standard function definitions 
-#include <fcntl.h>    // File control definitions 
-#include <errno.h>    // Error number definitions 
-#include <termios.h>  // POSIX terminal control definitions 
-#include <string.h>   // String function definitions 
+#include <stdio.h>    // Standard input/output definitions
+#include <unistd.h>   // UNIX standard function definitions
+#include <fcntl.h>    // File control definitions
+#include <errno.h>    // Error number definitions
+#include <termios.h>  // POSIX terminal control definitions
+#include <string.h>   // String function definitions (Sebgiles)
 #include <sys/ioctl.h>
 
 // uncomment this to debug reads
-//#define SERIALPORTDEBUG 
+//#define SERIALPORTDEBUG
 
 // takes the string name of the serial port (e.g. "/dev/tty.usbserial","COM1")
 // and a baud rate (bps) and connects to that port at that speed and 8N1.
@@ -25,15 +33,15 @@ int serialport_init(const char* serialport, int baud)
 {
     struct termios toptions;
     int fd;
-    
+
     //fd = open(serialport, O_RDWR | O_NOCTTY | O_NDELAY);
     fd = open(serialport, O_RDWR | O_NONBLOCK );
-    
+
     if (fd == -1)  {
         perror("serialport_init: Unable to open port ");
         return -1;
     }
-    
+
     //int iflags = TIOCM_DTR;
     //ioctl(fd, TIOCMBIS, &iflags);     // turn on DTR
     //ioctl(fd, TIOCMBIC, &iflags);    // turn off DTR
@@ -80,7 +88,7 @@ int serialport_init(const char* serialport, int baud)
     toptions.c_cc[VMIN]  = 0;
     toptions.c_cc[VTIME] = 0;
     //toptions.c_cc[VTIME] = 20;
-    
+
     tcsetattr(fd, TCSANOW, &toptions);
     if( tcsetattr(fd, TCSAFLUSH, &toptions) < 0) {
         perror("init_serialport: Couldn't set term attributes");
@@ -106,12 +114,12 @@ int serialport_writebyte( int fd, uint8_t b)
 }
 
 //
-int serialport_write(int fd, const char* str)
+int serialport_write(int fd, const char* str, int len)
 {
-    int len = strlen(str);
+    if (len<0) len = strlen(str); //small edit by Sebgiles
     int n = write(fd, str, len);
     if( n!=len ) {
-        perror("serialport_write: couldn't write whole string\n");
+        perror("serialport_write: couldn't write whole string/array\n");
         return -1;
     }
     return 0;
@@ -122,7 +130,7 @@ int serialport_read_until(int fd, char* buf, char until, int buf_max, int timeou
 {
     char b[1];  // read expects an array, so we give it a 1-byte array
     int i=0;
-    do { 
+    do {
         int n = read(fd, b, 1);  // read a char at a time
         if( n==-1) return -1;    // couldn't read
         if( n==0 ) {
@@ -131,10 +139,10 @@ int serialport_read_until(int fd, char* buf, char until, int buf_max, int timeou
             if( timeout==0 ) return -2;
             continue;
         }
-#ifdef SERIALPORTDEBUG  
+#ifdef SERIALPORTDEBUG
         printf("serialport_read_until: i=%d, n=%d b='%c'\n",i,n,b[0]); // debug
 #endif
-        buf[i] = b[0]; 
+        buf[i] = b[0];
         i++;
     } while( b[0] != until && i < buf_max && timeout>0 );
 
@@ -147,4 +155,47 @@ int serialport_flush(int fd)
 {
     sleep(2); //required to make flush work, for some reason
     return tcflush(fd, TCIOFLUSH);
+}
+
+//Addition by Sebastian Giles ==================================================
+int serialport_peek(int fd)
+{
+  static int n= -1;
+  if (n!=-1) return n;
+  char b[1];  // read expects an array, so we give it a 1-byte array
+
+  n = read(fd, b, 1);  // read a char
+  if( n!=1 ) return (n=-1); // (nothing to / couldn't)read
+  return (n=b[0]);
+}
+
+//Addition by Sebastian Giles
+int serialport_read(int fd)
+{
+  char b[1];  // read expects an array, so we give it a 1-byte array
+  int n = read(fd, b, 1);  // read a char
+  if( n!=1 ) return -1;    // (nothing to / couldn't)read
+  return b[0];
+}
+
+//Addition by Sebastian Giles
+int serialport_read_patient(int fd, char* buf, int len)
+{
+    char b[1];  // read expects an array, so we give it a 1-byte array
+    int i=0;
+    do {
+        int n = read(fd, b, 1);  // read a char at a time
+        if( n==-1) return -1;    // couldn't read
+        if( n==0 ) {
+            usleep( 1 * 1000 );  // wait 1 msec try again
+            continue;
+        }
+#ifdef SERIALPORTDEBUG
+        printf("serialport_read_patient: i=%d, n=%d b='%c'\n",i,n,b[0]); // debug
+#endif
+        buf[i] = b[0];
+        i++;
+    } while(i < len);
+
+    return 0;
 }
--- arduino-serial-lib.h
+++ arduino-serial-lib.h
@@ -3,19 +3,24 @@
 //
 // 2006-2013, Tod E. Kurt, http://todbot.com/blog/
 //
+// Functions added by Sebastian Giles: _read_patient, _peek, _read
+
 
 
 #ifndef __ARDUINO_SERIAL_LIB_H__
 #define __ARDUINO_SERIAL_LIB_H__
 
-#include <stdint.h>   // Standard types 
+#include <stdint.h>   // Standard types
 
 int serialport_init(const char* serialport, int baud);
 int serialport_close(int fd);
 int serialport_writebyte( int fd, uint8_t b);
-int serialport_write(int fd, const char* str);
+int serialport_write(int fd, const char* str, int len=-1);
 int serialport_read_until(int fd, char* buf, char until, int buf_max,int timeout);
 int serialport_flush(int fd);
 
-#endif
+int serialport_read_patient(int fd, char* buf, int len);
+int serialport_read(int fd);
+int serialport_peek(int fd);
 
+#endif
